ac00ad5c636c7a2a9b4c95d0efbdd25a24365d26
czen0002 <zc114789@gmail.com>
2021-12-09 21:32:48 +1100

    [CHENG] Initialize epl-dashboard-api

---
+++ src/main/java/io/czen/epldashboardapi/EplDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package io.czen.epldashboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplDashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplDashboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/czen/epldashboardapi/EplDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.czen.epldashboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplDashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
