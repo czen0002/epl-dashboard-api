1beb66bd0fc28be93413c89361d4523e5feb0a83
czen0002 <zc114789@gmail.com>
2022-05-20 00:02:00 +1000

    [CHENG] Add new TableTeam class

---
+++ src/main/java/io/czen/epldashboardapi/model/TableTeam.java
@@ -0,0 +1,45 @@
+package io.czen.epldashboardapi.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class TableTeam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private Integer played;
+    private Integer won;
+    private Integer drawn;
+    private Integer lost;
+    private Integer goalsFor;
+    private Integer goalsAgainst;
+    private Integer goalsDifference;
+    private Integer points;
+    private String season;
+
+    public TableTeam(String teamName, Integer won, Integer drawn, Integer lost, Integer goalsFor,
+                     Integer goalsAgainst, Integer goalsDifference, Integer points, String season) {
+        this.teamName = teamName;
+        this.played = 1;
+        this.won = won;
+        this.drawn = drawn;
+        this.lost = lost;
+        this.goalsFor = goalsFor;
+        this.goalsAgainst = goalsAgainst;
+        this.goalsDifference = goalsDifference;
+        this.points = points;
+        this.season = season;
+    }
+}
