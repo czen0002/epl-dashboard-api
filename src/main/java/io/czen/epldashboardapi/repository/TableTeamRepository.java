e5a3446f4a23495f0954bf0ac1c047360f262861
czen0002 <zc114789@gmail.com>
2022-05-24 23:40:14 +1000

    [CHENG] Update repositories

--- src/main/java/io/czen/epldashboardapi/repository/TableTeamRepository.java
+++ src/main/java/io/czen/epldashboardapi/repository/TableTeamRepository.java
@@ -1,2 +1,14 @@
-package io.czen.epldashboardapi.repository;public class TableTeamRepository {
+package io.czen.epldashboardapi.repository;
+
+import io.czen.epldashboardapi.model.TableTeam;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TableTeamRepository extends CrudRepository<TableTeam, Long> {
+
+    Optional<TableTeam> findByTeamName(String teamName);
+    Iterable<TableTeam> findAllByOrderByPointsDesc();
 }
--- src/main/java/io/czen/epldashboardapi/repository/TeamRepository.java
+++ src/main/java/io/czen/epldashboardapi/repository/TeamRepository.java
@@ -2,10 +2,11 @@ package io.czen.epldashboardapi.repository;
 
 import io.czen.epldashboardapi.model.Team;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TeamRepository extends CrudRepository<Team, Long> {
 
     Iterable<Team> findAllByOrderByTeamName();
