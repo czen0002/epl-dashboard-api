63ff9bf3cb5dda5cc9b2f6508aa5f473f192a474
czen0002 <zc114789@gmail.com>
2022-05-26 00:15:55 +1000

    [CHENG] Update MatchTeamWriterTest.java

--- src/test/java/io/czen/epldashboardapi/data/writer/MatchTeamWriterTest.java
+++ src/test/java/io/czen/epldashboardapi/data/writer/MatchTeamWriterTest.java
@@ -3,7 +3,6 @@ package io.czen.epldashboardapi.data.writer;
 import io.czen.epldashboardapi.model.MatchTeam;
 import io.czen.epldashboardapi.model.TableTeam;
 import io.czen.epldashboardapi.repository.TableTeamRepository;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +13,18 @@ import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class MatchTeamWriterTest {
 
-    private EntityManager entityManager;
-    private TableTeamRepository tableTeamRepository;
+    private final String SEASON = "2021-22";
+    private final String ARSENAL = "Arsenal";
+    private final String LEEDS = "Leeds";
+
+    private final EntityManager entityManager;
+    private final TableTeamRepository tableTeamRepository;
 
     @Autowired
     public MatchTeamWriterTest(
@@ -33,34 +38,34 @@ public class MatchTeamWriterTest {
     public void writeSuccessfully() throws Exception {
         MatchTeamWriter matchTeamHomeWriter = new MatchTeamWriter(entityManager);
         MatchTeam matchTeam1 = new MatchTeam(1, 0, 0, 3, 2, 1, 3);
-        matchTeam1.setTeamName("Arsenal");
-        matchTeam1.setSeason("2021-22");
+        matchTeam1.setTeamName(ARSENAL);
+        matchTeam1.setSeason(SEASON);
         MatchTeam matchTeam2 = new MatchTeam(0, 1, 0, 0, 0, 0, 1);
-        matchTeam2.setTeamName("Arsenal");
-        matchTeam2.setSeason("2021-22");
+        matchTeam2.setTeamName(ARSENAL);
+        matchTeam2.setSeason(SEASON);
         MatchTeam matchTeam3 = new MatchTeam(0, 0, 1, 1, 2, -1, 0);
-        matchTeam3.setTeamName("Arsenal");
-        matchTeam3.setSeason("2021-22");
+        matchTeam3.setTeamName(ARSENAL);
+        matchTeam3.setSeason(SEASON);
         MatchTeam matchTeam4 = new MatchTeam(0, 1, 0, 2, 2, 0, 1);
-        matchTeam4.setTeamName("Leeds");
-        matchTeam4.setSeason("2021-22");
-        List<MatchTeam> matchTeams = new ArrayList<MatchTeam>(List.of(matchTeam1, matchTeam2, matchTeam3, matchTeam4));
+        matchTeam4.setTeamName(LEEDS);
+        matchTeam4.setSeason(SEASON);
+        List<MatchTeam> matchTeams = new ArrayList<>(List.of(matchTeam1, matchTeam2, matchTeam3, matchTeam4));
         matchTeamHomeWriter.write(matchTeams);
 
-        TableTeam tableTeam1 = tableTeamRepository.findByTeamName("Arsenal").get();
-        Assert.assertEquals(Integer.valueOf(4), tableTeam1.getPoints());
-        Assert.assertEquals(Integer.valueOf(3), tableTeam1.getPlayed());
-        Assert.assertEquals(Integer.valueOf(0), tableTeam1.getGoalsDifference());
-        TableTeam tableTeam2 = tableTeamRepository.findByTeamName("Leeds").get();
-        Assert.assertEquals(Integer.valueOf(1), tableTeam2.getPoints());
-        Assert.assertEquals(Integer.valueOf(1), tableTeam2.getPlayed());
-        Assert.assertEquals(Integer.valueOf(0), tableTeam2.getGoalsDifference());
+        TableTeam tableTeam1 = tableTeamRepository.findByTeamName(ARSENAL).get();
+        assertEquals(Integer.valueOf(4), tableTeam1.getPoints());
+        assertEquals(Integer.valueOf(3), tableTeam1.getPlayed());
+        assertEquals(Integer.valueOf(0), tableTeam1.getGoalsDifference());
+        TableTeam tableTeam2 = tableTeamRepository.findByTeamName(LEEDS).get();
+        assertEquals(Integer.valueOf(1), tableTeam2.getPoints());
+        assertEquals(Integer.valueOf(1), tableTeam2.getPlayed());
+        assertEquals(Integer.valueOf(0), tableTeam2.getGoalsDifference());
 
         Iterable<TableTeam> tableTeams = tableTeamRepository.findAllByOrderByPointsDesc();
         List<TableTeam> tableTeamsList = new ArrayList<>();
         tableTeams.forEach(tableTeamsList::add);
-        Assert.assertEquals(2, tableTeamsList.size());
-        Assert.assertEquals("Arsenal", tableTeamsList.get(0).getTeamName());
-        Assert.assertEquals("Leeds", tableTeamsList.get(1).getTeamName());
+        assertEquals(2, tableTeamsList.size());
+        assertEquals(ARSENAL, tableTeamsList.get(0).getTeamName());
+        assertEquals(LEEDS, tableTeamsList.get(1).getTeamName());
     }
 }
