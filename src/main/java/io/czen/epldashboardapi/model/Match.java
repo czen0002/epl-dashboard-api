2d6226530d55dce12dc896f93721a74e7a27c0db
czen0002 <zc114789@gmail.com>
2022-05-27 00:25:27 +1000

    [CHENG] Add MatchServiceTest.java

--- src/main/java/io/czen/epldashboardapi/model/Match.java
+++ src/main/java/io/czen/epldashboardapi/model/Match.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
@@ -15,6 +17,7 @@ import java.time.LocalDate;
 public class Match {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String season;
     private LocalDate date;
---
+++ src/test/java/io/czen/epldashboardapi/data/service/MatchServiceTest.java
@@ -0,0 +1,81 @@
+package io.czen.epldashboardapi.data.service;
+
+import io.czen.epldashboardapi.model.Match;
+import io.czen.epldashboardapi.repository.MatchRepository;
+import io.czen.epldashboardapi.service.MatchService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MatchServiceTest {
+
+    private final String ARSENAL = "Arsenal";
+    private final String CHELSEA = "Chelsea";
+    private final String LEEDS = "Leeds";
+    private final String LIVERPOOL = "Liverpool";
+    private final String EVERTON = "Everton";
+    private final String HOME_WON = "H";
+    private final String HOME_DRAWN = "D";
+    private final String HOME_LOST = "A";
+    private final String SEASON = "2021-22";
+
+    private final MatchRepository matchRepository;
+    private MatchService matchService;
+
+    @Autowired
+    public MatchServiceTest(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
+    @BeforeAll
+    public void setup() {
+        Match match1 = new Match(ARSENAL, CHELSEA, HOME_WON);
+        match1.setSeason(SEASON);
+        Match match2 = new Match(ARSENAL, LEEDS, HOME_DRAWN);
+        match2.setSeason(SEASON);
+        Match match3 = new Match(ARSENAL, LIVERPOOL, HOME_LOST);
+        match3.setSeason(SEASON);
+        Match match4 = new Match(CHELSEA, LIVERPOOL, HOME_DRAWN);
+        match4.setSeason(SEASON);
+        Match[] matches = {match1, match2, match3, match4};
+        Iterable<Match> iterableMatches = Arrays.asList(matches);
+        matchRepository.saveAll(iterableMatches);
+        matchService = new MatchService(matchRepository);
+    }
+
+    @AfterAll
+    public void cleanUp() {
+        matchRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldGetMatchesBySeason() {
+        List<Match> matchesBySeason = matchService.getMatchesBySeason(ARSENAL, SEASON);
+        assertEquals(3, matchesBySeason.size());
+    }
+
+    @Test
+    public void shouldGetEmptyMatchesBySeason() {
+        List<Match> matchesBySeason = matchService.getMatchesBySeason(EVERTON, SEASON);
+        assertEquals(0, matchesBySeason.size());
+    }
+
+    @Test
+    public void shouldGetTwoLatestMatchesByTeam() {
+        List<Match> latestTwoMatches = matchService.getLatestMatchesByTeam(ARSENAL, 2);
+        assertEquals(2, latestTwoMatches.size());
+    }
+}
