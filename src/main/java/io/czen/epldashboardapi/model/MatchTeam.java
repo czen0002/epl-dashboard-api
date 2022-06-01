9b60d253264d9f3c509126439006eb56cf7d75da
czen0002 <zc114789@gmail.com>
2022-06-02 00:06:18 +1000

    [CHENG] Remove Entity from MatchTeam

--- src/main/java/io/czen/epldashboardapi/model/MatchTeam.java
+++ src/main/java/io/czen/epldashboardapi/model/MatchTeam.java
@@ -4,20 +4,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Getter
 @Setter
-@Entity
 @NoArgsConstructor
 public class MatchTeam {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
     private String teamName;
     private Integer won;
     private Integer drawn;
