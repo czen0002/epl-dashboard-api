416c6f0d7a97d8089dc68a4b4e4a823f82e2eb42
czen0002 <zc114789@gmail.com>
2022-06-05 20:35:16 +1000

    [update-test][CHENG] Update converter test

--- src/test/java/io/czen/epldashboardapi/util/TeamConverterTest.java
+++ src/test/java/io/czen/epldashboardapi/util/TeamConverterTest.java
@@ -4,16 +4,34 @@ import io.czen.epldashboardapi.entity.TeamEntity;
 import io.czen.epldashboardapi.model.Team;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("squid:S5786")
 public class TeamConverterTest {
 
+    private final String ARSENAL = "Arsenal";
+    private final String CHELSEA = "Chelsea";
+
     @Test
-    public void convertTeamSuccessfully() {
-        TeamEntity teamEntity = new TeamEntity("Arsenal");
+    public void convertTeamEntitySuccessfully() {
+        TeamEntity teamEntity = new TeamEntity(ARSENAL);
         Team team = TeamConverter.convertTeamEntity(teamEntity);
 
-        assertEquals("Arsenal", team.getTeamName());
+        assertEquals(ARSENAL, team.getTeamName());
+    }
+
+    @Test
+    public void convertTeamEntitiesSuccessfully() {
+        TeamEntity teamEntity1 = new TeamEntity(ARSENAL);
+        TeamEntity teamEntity2 = new TeamEntity(CHELSEA);
+        List<TeamEntity> teamEntities = Arrays.asList(teamEntity1, teamEntity2);
+        List<Team> result = TeamConverter.convertTeamEntities(teamEntities);
+
+        assertEquals(2, result.size());
+        assertEquals(ARSENAL, result.get(0).getTeamName());
+        assertEquals(CHELSEA, result.get(1).getTeamName());
     }
 }
