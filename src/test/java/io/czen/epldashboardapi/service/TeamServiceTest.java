e74ac2cb00207e20bd81d0400ace516f906e15a5
czen0002 <zc114789@gmail.com>
2022-06-05 22:05:15 +1000

    [update-test][CHENG] Update service tests

--- src/test/java/io/czen/epldashboardapi/service/TeamServiceTest.java
+++ src/test/java/io/czen/epldashboardapi/service/TeamServiceTest.java
@@ -1,26 +1,26 @@
 package io.czen.epldashboardapi.service;
 
+import io.czen.epldashboardapi.entity.MatchEntity;
 import io.czen.epldashboardapi.entity.TeamEntity;
-import io.czen.epldashboardapi.model.Match;
 import io.czen.epldashboardapi.model.Team;
+import io.czen.epldashboardapi.repository.MatchRepository;
 import io.czen.epldashboardapi.repository.TeamRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SuppressWarnings("squid:S5786")
 public class TeamServiceTest {
 
@@ -28,26 +28,46 @@ public class TeamServiceTest {
     private final String CHELSEA = "Chelsea";
     private final String LEEDS = "Leeds";
     private final String LIVERPOOL = "Liverpool";
+    private final String EVERTON = "Everton";
 
-    @InjectMocks
+    private final TeamRepository teamRepository;
+    private final MatchRepository matchRepository;
+    private MatchService matchService;
     private TeamService teamService;
 
-    @Mock
-    private TeamRepository teamRepository;
+    @Autowired
+    public TeamServiceTest(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
 
-    @Mock
-    private MatchService matchService;
-
-    @Test
-    public void shouldGetAllTeamsOrderByTeamName() {
+    @BeforeAll
+    public void setup() {
         TeamEntity teamEntity1 = new TeamEntity(ARSENAL);
         TeamEntity teamEntity2 = new TeamEntity(LEEDS);
         TeamEntity teamEntity3 = new TeamEntity(LIVERPOOL);
         TeamEntity teamEntity4 = new TeamEntity(CHELSEA);
-        Iterable<TeamEntity> iterableTeams = Arrays.asList(teamEntity1, teamEntity4, teamEntity2, teamEntity3);
-        when(teamRepository.findAllByOrderByTeamName()).thenReturn(iterableTeams);
-        List<Team> result = teamService.getAllTeamsOrderByTeamName();
+        Iterable<TeamEntity> iterableTeams = Arrays.asList(teamEntity1, teamEntity2, teamEntity3, teamEntity4);
+        teamRepository.saveAll(iterableTeams);
 
+        MatchEntity matchEntity1 = new MatchEntity(ARSENAL, CHELSEA, "W");
+        MatchEntity matchEntity2 = new MatchEntity(ARSENAL, LIVERPOOL, "W");
+        MatchEntity matchEntity3 = new MatchEntity(ARSENAL, LEEDS, "D");
+        Iterable<MatchEntity> iterableMatches = Arrays.asList(matchEntity1, matchEntity2, matchEntity3);
+        matchRepository.saveAll(iterableMatches);
+
+        matchService = new MatchService(matchRepository);
+        teamService = new TeamService(teamRepository, matchService);
+    }
+
+    @AfterAll
+    public void cleanUp() {
+        teamRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldGetAllTeamsOrderByTeamName() {
+        List<Team> result = teamService.getAllTeamsOrderByTeamName();
         assertEquals(4, result.size());
         assertEquals(ARSENAL, result.get(0).getTeamName());
         assertEquals(CHELSEA, result.get(1).getTeamName());
@@ -57,41 +77,26 @@ public class TeamServiceTest {
 
     @Test
     public void shouldGetTeam() {
-        TeamEntity teamEntity = new TeamEntity(ARSENAL);
-        when(teamRepository.findByTeamName(anyString())).thenReturn(Optional.of(teamEntity));
-        Team result = teamService.getTeam(anyString());
-
-        assertEquals(ARSENAL, result.getTeamName());
+        Team result = teamService.getTeam(ARSENAL);
+        assertNotNull(result);
     }
 
     @Test
-    public void shouldGetNull() {
-        when(teamRepository.findByTeamName(anyString())).thenReturn(Optional.empty());
-        Team result = teamService.getTeam(anyString());
-
+    public void shouldGetTeamNull() {
+        Team result = teamService.getTeam(EVERTON);
         assertNull(result);
     }
 
     @Test
     public void shouldGetTeamWithMatches() {
-        TeamEntity teamEntity = new TeamEntity(ARSENAL);
-        Match match1 = new Match(ARSENAL, CHELSEA, "W");
-        Match match2 = new Match(ARSENAL, LIVERPOOL, "W");
-        Match match3 = new Match(ARSENAL, LEEDS, "D");
-        List<Match> matches = Arrays.asList(match1, match2, match3);
-        when(teamRepository.findByTeamName(anyString())).thenReturn(Optional.of(teamEntity));
-        when(matchService.getLatestMatchesByTeam(anyString(), anyInt())).thenReturn(matches);
         Team result = teamService.getTeamWithMatches(ARSENAL, 3);
-
         assertEquals(ARSENAL, result.getTeamName());
         assertEquals(3, result.getMatches().size());
     }
 
     @Test
-    public void shouldGetNullTeam() {
-        when(teamRepository.findByTeamName(anyString())).thenReturn(Optional.empty());
-        Team result = teamService.getTeamWithMatches(ARSENAL, 3);
-
+    public void shouldGetTeamWithMatchesNull() {
+        Team result = teamService.getTeamWithMatches(EVERTON, 3);
         assertNull(result);
     }
 }
