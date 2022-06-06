8e106b964cd6af453eaf667ce6d465de5e898cc7
czen0002 <zc114789@gmail.com>
2022-06-06 23:07:06 +1000

    [ranking-table-endpoint][CHENG] Add ranking table endpoint

--- src/main/java/io/czen/epldashboardapi/controller/TeamController.java
+++ src/main/java/io/czen/epldashboardapi/controller/TeamController.java
@@ -1,8 +1,10 @@
 package io.czen.epldashboardapi.controller;
 
 import io.czen.epldashboardapi.model.Match;
+import io.czen.epldashboardapi.model.RankingTableTeam;
 import io.czen.epldashboardapi.model.Team;
 import io.czen.epldashboardapi.service.MatchService;
+import io.czen.epldashboardapi.service.RankingTableTeamService;
 import io.czen.epldashboardapi.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +23,14 @@ public class TeamController {
 
     private final TeamService teamService;
     private final MatchService matchService;
+    private final RankingTableTeamService rankingTableTeamService;
 
     @Autowired
-    public TeamController(TeamService teamService, MatchService matchService) {
+    public TeamController(TeamService teamService, MatchService matchService,
+                          RankingTableTeamService rankingTableTeamService) {
         this.teamService = teamService;
         this.matchService = matchService;
+        this.rankingTableTeamService = rankingTableTeamService;
     }
 
     @GetMapping(value = "/team")
@@ -41,5 +46,10 @@ public class TeamController {
     @GetMapping(value = "/team/{teamName}")
     public Team getTeam(@PathVariable String teamName, @RequestParam int count) {
         return teamService.getTeamWithMatches(teamName, count);
+    }
+
+    @GetMapping(value = "/table/{season}")
+    public List<RankingTableTeam> getRankingTableInSeason(@PathVariable String season) {
+        return rankingTableTeamService.getRankingTableTeamsBySeason(season);
     }
 }
--- src/test/java/io/czen/epldashboardapi/controller/TeamControllerTest.java
+++ src/test/java/io/czen/epldashboardapi/controller/TeamControllerTest.java
@@ -1,8 +1,10 @@
 package io.czen.epldashboardapi.controller;
 
 import io.czen.epldashboardapi.model.Match;
+import io.czen.epldashboardapi.model.RankingTableTeam;
 import io.czen.epldashboardapi.model.Team;
 import io.czen.epldashboardapi.service.MatchService;
+import io.czen.epldashboardapi.service.RankingTableTeamService;
 import io.czen.epldashboardapi.service.TeamService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +24,12 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 @SuppressWarnings("squid:S5786")
 public class TeamControllerTest {
+    
+    private final String ARSENAL = "Arsenal";
+    private final String CHELSEA = "Chelsea";
+    private final String LIVERPOOL = "Liverpool";
+    private final String HOME_WON = "W";
+    private final String SEASON = "2021-22";
 
     @InjectMocks
     private TeamController teamController;
@@ -32,52 +40,77 @@ public class TeamControllerTest {
     @Mock
     private MatchService matchService;
 
+    @Mock
+    private RankingTableTeamService rankingTableTeamService;
+
     @Test
     public void shouldGetAllTeamsOrderByTeamName() {
-        Team team1 = new Team("Arsenal");
-        Team team2 = new Team("Chelsea");
+        Team team1 = new Team(ARSENAL);
+        Team team2 = new Team(CHELSEA);
         List<Team> teams = Arrays.asList(team1, team2);
         when(teamService.getAllTeamsOrderByTeamName()).thenReturn(teams);
         List<Team> result = teamController.getAllTeamOrderByTeamName();
 
-        assertEquals("Arsenal", result.get(0).getTeamName());
-        assertEquals("Chelsea", result.get(1).getTeamName());
+        assertEquals(ARSENAL, result.get(0).getTeamName());
+        assertEquals(CHELSEA, result.get(1).getTeamName());
     }
 
     @Test
     public void shouldGetMatchesForTeamInSeason() {
-        Match match1 = new Match("Arsenal", "Chelsea", "W");
-        Match match2 = new Match("Arsenal", "Liverpool", "W");
+        Match match1 = new Match(ARSENAL, CHELSEA, HOME_WON);
+        Match match2 = new Match(ARSENAL, LIVERPOOL, HOME_WON);
         List<Match> matches = Arrays.asList(match1, match2);
         when(matchService.getMatchesBySeason(anyString(), anyString())).thenReturn(matches);
-        List<Match> results = teamController.getMatchesForTeamInSeason("Arsenal", "2021-22");
+        List<Match> results = teamController.getMatchesForTeamInSeason(ARSENAL, SEASON);
 
         assertEquals(2, results.size());
-        assertEquals("Arsenal", results.get(0).getHomeTeam());
-        assertEquals("Chelsea", results.get(0).getAwayTeam());
-        assertEquals("W", results.get(0).getFullTimeResult());
+        assertEquals(ARSENAL, results.get(0).getHomeTeam());
+        assertEquals(CHELSEA, results.get(0).getAwayTeam());
+        assertEquals(HOME_WON, results.get(0).getFullTimeResult());
     }
 
     @Test
     public void shouldGetTeam() {
-        Match match1 = new Match("Arsenal", "Chelsea", "W");
-        Match match2 = new Match("Arsenal", "Liverpool", "W");
-        Team team = new Team("Arsenal");
+        Match match1 = new Match(ARSENAL, CHELSEA, HOME_WON);
+        Match match2 = new Match(ARSENAL, LIVERPOOL, HOME_WON);
+        Team team = new Team(ARSENAL);
         List<Match> matches = Arrays.asList(match1, match2);
         team.setMatches(matches);
         when(teamService.getTeamWithMatches(anyString(), anyInt())).thenReturn(team);
-        Team result = teamController.getTeam("Arsenal", 2);
+        Team result = teamController.getTeam(ARSENAL, 2);
 
         assertEquals(2, result.getMatches().size());
-        assertEquals("Arsenal", result.getMatches().get(1).getHomeTeam());
-        assertEquals("Liverpool", result.getMatches().get(1).getAwayTeam());
+        assertEquals(ARSENAL, result.getMatches().get(1).getHomeTeam());
+        assertEquals(LIVERPOOL, result.getMatches().get(1).getAwayTeam());
         assertEquals("W", result.getMatches().get(1).getFullTimeResult());
     }
 
-    @Test void shouldGetTeamNull() {
+    @Test
+    public void shouldGetTeamNull() {
         when(teamService.getTeamWithMatches(anyString(), anyInt())).thenReturn(null);
-        Team result = teamController.getTeam("Arsenal", 2);
+        Team result = teamController.getTeam(ARSENAL, 2);
 
         assertNull(result);
+    }
+
+    @Test
+    public void shouldGetRankingTableBySeason() {
+        RankingTableTeam rankingTableTeam1 = new RankingTableTeam();
+        rankingTableTeam1.setTeamName(ARSENAL);
+        rankingTableTeam1.setSeason(SEASON);
+        RankingTableTeam rankingTableTeam2 = new RankingTableTeam();
+        rankingTableTeam2.setTeamName(CHELSEA);
+        rankingTableTeam2.setSeason(SEASON);
+        RankingTableTeam rankingTableTeam3 = new RankingTableTeam();
+        rankingTableTeam3.setTeamName(LIVERPOOL);
+        rankingTableTeam3.setSeason(SEASON);
+        List<RankingTableTeam> rankingTableTeams = Arrays.asList(rankingTableTeam1, rankingTableTeam2, rankingTableTeam3);
+        when(rankingTableTeamService.getRankingTableTeamsBySeason(anyString())).thenReturn(rankingTableTeams);
+        List<RankingTableTeam> result = teamController.getRankingTableInSeason(SEASON);
+        
+        assertEquals(3, result.size());
+        assertEquals(ARSENAL, result.get(0).getTeamName());
+        assertEquals(CHELSEA, result.get(1).getTeamName());
+        assertEquals(LIVERPOOL, result.get(2).getTeamName());
     }
 }
