dbafde18479421c65eb7c183cba692f5c59f1c2b
czen0002 <zc114789@gmail.com>
2022-06-07 23:09:28 +1000

    [match-writer][CHENG] Update MatchTeamWriter.java

--- src/main/java/io/czen/epldashboardapi/data/writer/MatchTeamWriter.java
+++ src/main/java/io/czen/epldashboardapi/data/writer/MatchTeamWriter.java
@@ -21,13 +21,13 @@ public class MatchTeamWriter implements ItemWriter<MatchTeam> {
     @Override
     public void write(List<? extends MatchTeam> matchTeams) {
         for (MatchTeam matchTeam : matchTeams) {
-            updateRankingTableTeam(matchTeam);
+            saveRankingTableTeamEntity(matchTeam);
         }
     }
 
-    private void updateRankingTableTeam(MatchTeam newMatchTeam) {
-        String teamName = newMatchTeam.getTeamName();
-        String season = newMatchTeam.getSeason();
+    private void saveRankingTableTeamEntity(MatchTeam matchTeam) {
+        String teamName = matchTeam.getTeamName();
+        String season = matchTeam.getSeason();
         TypedQuery<RankingTableTeamEntity> query = em.createQuery(
                 "SELECT rtt FROM RankingTableTeamEntity rtt WHERE rtt.teamName = :teamName and rtt.season = :season", RankingTableTeamEntity.class);
         RankingTableTeamEntity rankingTableTeamEntity = query
@@ -40,23 +40,23 @@ public class MatchTeamWriter implements ItemWriter<MatchTeam> {
             rankingTableTeamEntity = new RankingTableTeamEntity(
                     teamName,
                     1,
-                    newMatchTeam.getWon(),
-                    newMatchTeam.getDrawn(),
-                    newMatchTeam.getLost(),
-                    newMatchTeam.getGoalsFor(),
-                    newMatchTeam.getGoalsAgainst(),
-                    newMatchTeam.getGoalsDifference(),
-                    newMatchTeam.getPoints(),
+                    matchTeam.getWon(),
+                    matchTeam.getDrawn(),
+                    matchTeam.getLost(),
+                    matchTeam.getGoalsFor(),
+                    matchTeam.getGoalsAgainst(),
+                    matchTeam.getGoalsDifference(),
+                    matchTeam.getPoints(),
                     season);
         } else {
             rankingTableTeamEntity.setPlayed(rankingTableTeamEntity.getPlayed() + 1);
-            rankingTableTeamEntity.setWon(rankingTableTeamEntity.getWon() + newMatchTeam.getWon());
-            rankingTableTeamEntity.setDrawn(rankingTableTeamEntity.getDrawn() + newMatchTeam.getDrawn());
-            rankingTableTeamEntity.setLost(rankingTableTeamEntity.getLost() + newMatchTeam.getLost());
-            rankingTableTeamEntity.setGoalsFor(rankingTableTeamEntity.getGoalsFor() + newMatchTeam.getGoalsFor());
-            rankingTableTeamEntity.setGoalsAgainst(rankingTableTeamEntity.getGoalsAgainst() + newMatchTeam.getGoalsAgainst());
-            rankingTableTeamEntity.setGoalsDifference(rankingTableTeamEntity.getGoalsDifference() + newMatchTeam.getGoalsDifference());
-            rankingTableTeamEntity.setPoints(rankingTableTeamEntity.getPoints() + newMatchTeam.getPoints());
+            rankingTableTeamEntity.setWon(rankingTableTeamEntity.getWon() + matchTeam.getWon());
+            rankingTableTeamEntity.setDrawn(rankingTableTeamEntity.getDrawn() + matchTeam.getDrawn());
+            rankingTableTeamEntity.setLost(rankingTableTeamEntity.getLost() + matchTeam.getLost());
+            rankingTableTeamEntity.setGoalsFor(rankingTableTeamEntity.getGoalsFor() + matchTeam.getGoalsFor());
+            rankingTableTeamEntity.setGoalsAgainst(rankingTableTeamEntity.getGoalsAgainst() + matchTeam.getGoalsAgainst());
+            rankingTableTeamEntity.setGoalsDifference(rankingTableTeamEntity.getGoalsDifference() + matchTeam.getGoalsDifference());
+            rankingTableTeamEntity.setPoints(rankingTableTeamEntity.getPoints() + matchTeam.getPoints());
         }
         em.persist(rankingTableTeamEntity);
     }
