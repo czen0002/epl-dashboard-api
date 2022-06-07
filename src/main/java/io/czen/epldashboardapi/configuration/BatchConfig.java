b6d113e7e62866afc4d8bdaf58d0a37647d36549
czen0002 <zc114789@gmail.com>
2022-06-08 00:31:33 +1000

    [match-writer][CHENG] Update match data

--- src/main/java/io/czen/epldashboardapi/configuration/BatchConfig.java
+++ src/main/java/io/czen/epldashboardapi/configuration/BatchConfig.java
@@ -6,7 +6,8 @@ import io.czen.epldashboardapi.data.processor.MatchDataProcessor;
 import io.czen.epldashboardapi.data.processor.TableTeamAwayProcessor;
 import io.czen.epldashboardapi.data.processor.TableTeamHomeProcessor;
 import io.czen.epldashboardapi.data.writer.MatchTeamWriter;
-import io.czen.epldashboardapi.entity.MatchEntity;
+import io.czen.epldashboardapi.data.writer.MatchWriter;
+import io.czen.epldashboardapi.model.Match;
 import io.czen.epldashboardapi.model.MatchTeam;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -15,9 +16,6 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -27,13 +25,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
-    private final String[] FIELD_NAMES = new String[] { "id", "season", "date", "homeTeam", "awayTeam",
+    private final String[] FIELD_NAMES = new String[] {"season", "date", "homeTeam", "awayTeam",
             "fullTimeHomeTeamGoals", "fullTimeAwayTeamGoals", "fullTimeResult", "halfTimeHomeTeamGoals",
             "halfTimeAwayTeamGoals", "halfTimeResult", "referee", "homeTeamShots", "awayTeamShots",
             "homeTeamShotsOnTarget", "awayTeamShotsOnTarget", "homeTeamCorners", "awayTeamCorners",
@@ -74,29 +71,35 @@ public class BatchConfig {
         return new TableTeamAwayProcessor();
     }
 
-    @Bean
-    public JdbcBatchItemWriter<MatchEntity> writeMatch(DataSource dataSource) {
-        return new JdbcBatchItemWriterBuilder<MatchEntity>()
-                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-                .sql("INSERT INTO match_entity(id, season, date, home_team, away_team, full_time_home_team_goals, " +
-                        "full_time_away_team_goals, full_time_result, half_time_home_team_goals, half_time_away_team_goals, " +
-                        "half_time_result, referee, home_team_shots, away_team_shots, home_team_shots_on_target, " +
-                        "away_team_shots_on_target, home_team_corners, away_team_corners, home_team_fouls_committed, " +
-                        "away_team_fouls_committed, home_team_yellow_cards, away_team_yellow_cards, home_team_red_cards, " +
-                        "away_team_red_cards) VALUES (:id, :season, :date, :homeTeam, :awayTeam, :fullTimeHomeTeamGoals," +
-                        " :fullTimeAwayTeamGoals, :fullTimeResult, :halfTimeHomeTeamGoals, :halfTimeAwayTeamGoals, " +
-                        ":halfTimeResult, :referee, :homeTeamShots, :awayTeamShots, :homeTeamShotsOnTarget, " +
-                        ":awayTeamShotsOnTarget, :homeTeamCorners, :awayTeamCorners, :homeTeamFoulsCommitted, " +
-                        ":awayTeamFoulsCommitted, :homeTeamYellowCards, :awayTeamYellowCards, :homeTeamRedCards, " +
-                        ":awayTeamRedCards)")
-                .dataSource(dataSource)
-                .build();
-    }
+//    @Bean
+//    public JdbcBatchItemWriter<MatchEntity> writeMatch(DataSource dataSource) {
+//        return new JdbcBatchItemWriterBuilder<MatchEntity>()
+//                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+//                .sql("INSERT INTO match_entity(id, season, date, home_team, away_team, full_time_home_team_goals, " +
+//                        "full_time_away_team_goals, full_time_result, half_time_home_team_goals, half_time_away_team_goals, " +
+//                        "half_time_result, referee, home_team_shots, away_team_shots, home_team_shots_on_target, " +
+//                        "away_team_shots_on_target, home_team_corners, away_team_corners, home_team_fouls_committed, " +
+//                        "away_team_fouls_committed, home_team_yellow_cards, away_team_yellow_cards, home_team_red_cards, " +
+//                        "away_team_red_cards) VALUES (:id, :season, :date, :homeTeam, :awayTeam, :fullTimeHomeTeamGoals," +
+//                        " :fullTimeAwayTeamGoals, :fullTimeResult, :halfTimeHomeTeamGoals, :halfTimeAwayTeamGoals, " +
+//                        ":halfTimeResult, :referee, :homeTeamShots, :awayTeamShots, :homeTeamShotsOnTarget, " +
+//                        ":awayTeamShotsOnTarget, :homeTeamCorners, :awayTeamCorners, :homeTeamFoulsCommitted, " +
+//                        ":awayTeamFoulsCommitted, :homeTeamYellowCards, :awayTeamYellowCards, :homeTeamRedCards, " +
+//                        ":awayTeamRedCards)")
+//                .dataSource(dataSource)
+//                .build();
+//    }
 
     @Bean
     @Autowired
     public MatchTeamWriter writeMatchTeam(EntityManager entityManager) {
         return new MatchTeamWriter(entityManager);
+    }
+
+    @Bean
+    @Autowired
+    public MatchWriter writerMatch(EntityManager entityManager) {
+        return new MatchWriter(entityManager);
     }
 
     @Bean
@@ -112,11 +115,22 @@ public class BatchConfig {
                 .build();
     }
 
+//    @Bean
+//    public Step writeMatchStep(ItemProcessor<MatchInput, MatchEntity> matchDataProcessor,
+//                               JdbcBatchItemWriter<MatchEntity> writeMatch) {
+//        return stepBuilderFactory.get("writeMatchStep")
+//                .<MatchInput, MatchEntity> chunk(10)
+//                .reader(reader())
+//                .processor(matchDataProcessor)
+//                .writer(writeMatch)
+//                .build();
+//    }
+
     @Bean
-    public Step writeMatchStep(ItemProcessor<MatchInput, MatchEntity> matchDataProcessor,
-                               JdbcBatchItemWriter<MatchEntity> writeMatch) {
+    public Step writeMatchStep(ItemProcessor<MatchInput, Match> matchDataProcessor,
+                               MatchWriter writeMatch) {
         return stepBuilderFactory.get("writeMatchStep")
-                .<MatchInput, MatchEntity> chunk(10)
+                .<MatchInput, Match> chunk(10)
                 .reader(reader())
                 .processor(matchDataProcessor)
                 .writer(writeMatch)
